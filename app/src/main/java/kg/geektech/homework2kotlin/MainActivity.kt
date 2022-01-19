542305e827583513a5712ebadbdaef298f373e71
aydaraktan <aydar.aktan@mail.ru>
2022-01-19 23:09:33 +0600

    new3

--- app/src/main/java/kg/geektech/homework2kotlin/MainActivity.kt
+++ app/src/main/java/kg/geektech/homework2kotlin/MainActivity.kt
@@ -3,17 +3,16 @@ package kg.geektech.homework2kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kg.geektech.homework2kotlin.databinding.ActivityMainBinding
-import kg.geektech.homework2kotlin.databinding.ActivityMainBinding.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val urls = mutableListOf("https://yablyk.com/wp-content/uploads/2018/04/shoten-links.jpg","https://bipbap.ru/wp-content/uploads/2017/04/priroda_kartinki_foto_03.jpg","https://bipbap.ru/wp-content/uploads/2017/04/kuukktsk.jpg",
+        var urls = mutableListOf("https://yablyk.com/wp-content/uploads/2018/04/shoten-links.jpg","https://bipbap.ru/wp-content/uploads/2017/04/priroda_kartinki_foto_03.jpg","https://bipbap.ru/wp-content/uploads/2017/04/kuukktsk.jpg",
         "https://russianblogs.com/images/103/69f223e82b33c342199241d463664907.png","https://ktonanovenkogo.ru/image/sokrashchenie-ssilok.jpg")
 
         binding.random.setOnClickListener {
@@ -25,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.submit.setOnClickListener {
             if(url.isBlank()){
-                showToast(getString(R.string.url))
+                showToast(getString(R.string.inputUrl))
             }else{
             urls.add(url.toString())
             binding.editText.text = null}
